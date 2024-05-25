176ab16690918ae03896e7fff6669f63eb1452d7
Galvanos99 <cezary.bomba@gmail.com>
2024-05-25 15:05:32 +0200

    cb3

--- web_app/Flask_SQLite_example/app/models.py
+++ web_app/Flask_SQLite_example/app/models.py
@@ -1,9 +1,10 @@
 from app import db
 
-class User(db.Model):
+class Teacher(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    subject = db.Column(db.String(120), unique=True, nullable=False)
+    time = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
--- web_app/Flask_SQLite_example/app/routes.py
+++ web_app/Flask_SQLite_example/app/routes.py
@@ -1,29 +1,30 @@
 from flask import render_template, request, redirect, url_for
 from app import app, db
-from app.models import User
+from app.models import Teacher
 
 @app.route('/')
 def index():
-    users = User.query.all()
+    users = Teacher.query.all()
     return render_template('index.html', users=users)
 
 @app.route('/add', methods=['GET', 'POST'])
-def add_user():
+def add_teacher():
     if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        if username and email:
-            new_user = User(username=username, email=email)
-            db.session.add(new_user)
+        name = request.form['name']
+        subject = request.form['subject']
+        time = request.form['time']
+        if name and subject and time:
+            new_teacher = Teacher(name=name, subject=subject, time = time)
+            db.session.add(new_teacher)
             db.session.commit()
             return redirect(url_for('index'))
-    return render_template('add_user.html')
+    return render_template('add_teacher.html')
 
 @app.route('/delete', methods=['GET', 'POST'])
 def delete_user():
     if request.method == 'POST':
         user_id = request.form['user_id']
-        user = User.query.get(user_id)
+        user = Teacher.query.get(user_id)
         if user:
             db.session.delete(user)
             db.session.commit()
